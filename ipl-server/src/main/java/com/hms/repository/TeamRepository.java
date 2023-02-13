90e7186d68b5280c85b4d2db6fe02995ecf76a49
Sameera Hewamadduma <sameerahewamaddumage@gmail.com>
2023-02-13 18:55:34 +0530

    delete server

--- ipl-server/src/main/java/com/hms/repository/TeamRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hms.repository;
-
-import com.hms.model.Team;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TeamRepository extends CrudRepository <Team, Long> {
-
-    Team findByTeamName(String teamName);
-}
